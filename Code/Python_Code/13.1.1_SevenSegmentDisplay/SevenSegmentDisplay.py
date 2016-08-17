31ddcf22d2b9e42054f7ca745a2d31a35f9b6f66
Suhaylzhao <838119509@qq.com>
2016-08-17 19:05:22 +0800

    fix some mistake in project 12.1.1 python code
    "LightWater" was modified to "LightWater02"

--- Code/Python_Code/13.1.1_SevenSegmentDisplay/SevenSegmentDisplay.py
+++ Code/Python_Code/13.1.1_SevenSegmentDisplay/SevenSegmentDisplay.py
@@ -14,7 +14,7 @@ MSBFIRST = 2
 dataPin   = 11		#DS Pin of 74HC595(Pin14)
 latchPin  = 13		#ST_CP Pin of 74HC595(Pin12)
 clockPin = 15		#CH_CP Pin of 74HC595(Pin11)
-#SevenSegmentDisplay display the character “0”- “F” successively
+#SevenSegmentDisplay display the character "0"- "F"successively
 num = [0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90,0x88,0x83,0xc6,0xa1,0x86,0x8e]
 def setup():
 	GPIO.setmode(GPIO.BOARD)    # Number GPIOs by its physical location
@@ -40,7 +40,7 @@ def loop():
 			time.sleep(0.5)
 		for i in range(0,len(num)):
 			GPIO.output(latchPin,GPIO.LOW)
-			shiftOut(dataPin,clockPin,MSBFIRST,num[i]&0x7f)#Use “&0x7f”to display the decimal point.
+			shiftOut(dataPin,clockPin,MSBFIRST,num[i]&0x7f)#Use "&0x7f"to display the decimal point.
 			GPIO.output(latchPin,GPIO.HIGH)
 			time.sleep(0.5)
 
