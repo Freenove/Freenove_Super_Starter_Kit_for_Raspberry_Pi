5218f96ddb653e6ce901619c9138e9d16fa3eb25
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-07-01 10:21:23 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here.
-from datetime import datetime
-import os
-import pathlib
-import sys
-
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
-os.system("rm -r freenove_Kit")
-os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Super_Starter_Kit_for_Raspberry_Pi freenove_Kit")
-
-project = "fnk0019-docs"
 # <!!!BEGIN!!!>
 copyright = '2016-2025, Freenove'
 author = 'Freenove'
